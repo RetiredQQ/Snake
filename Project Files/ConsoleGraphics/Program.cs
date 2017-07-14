2cc4ea3771aee75a66043f0deb1025e068166eb3
RetiredQQ <retiredqq@gmail.com>
2017-07-14 19:22:27 +0800

    Update Program.cs
    Simplify some codes.

--- Project Files/ConsoleGraphics/Program.cs
+++ Project Files/ConsoleGraphics/Program.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Drawing;
-using System.Windows;
-using System.Collections.Generic;
-using System.Collections;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace ConsoleGraphics
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             game:
             {
@@ -47,46 +41,40 @@ namespace ConsoleGraphics
                 int pelletY = 0;
 
                 int[] xPoints;
-                xPoints = new int[8] { 20, 19, 18, 17, 16, 15, 14, 13 };
+                xPoints = new [] { 20, 19, 18, 17, 16, 15, 14, 13 };
                 int[] yPoints;
-                yPoints = new int[8] { 20, 20, 20, 20, 20, 20, 20, 20 };
+                yPoints = new [] { 20, 20, 20, 20, 20, 20, 20, 20 };
 
 
                 while (alive)
                 {
-                    if (pelletOn == false)
-                    {
-                        bool collide = false;
-                        pelletOn = true;
-                        pelletX = rnd.Next(4, Console.WindowWidth - 4);
-                        pelletY = rnd.Next(4, Console.WindowHeight - 4);
+					if (!pelletOn)
+					{
+						bool collide = false;
+						pelletOn = true;
+						pelletX = rnd.Next(4, Console.WindowWidth - 4);
+						pelletY = rnd.Next(4, Console.WindowHeight - 4);
 
-                        for (int l = (xPoints.Length - 1); l > 1; l--)
-                        {
-                            if (xPoints[l] == pelletX & yPoints[l] == pelletY)
-                            {
-                                collide = true;
-                            }
-                        }
-                        if (collide == true)
-                        {
-                            pelletOn = false;
-                            break;
-                        }
-                        else
-                        {
-                            Console.SetCursorPosition(pelletX, pelletY);
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.BackgroundColor = bgColor;
-                            Console.Write("#");
-                            pelletOn = true;
-                        }
+						for (int l = (xPoints.Length - 1); l > 1; l--)
+						{
+							collide |= xPoints[l] == pelletX & yPoints[l] == pelletY;
+						}
+						if (collide)
+						{
+							pelletOn = false;
+							break;
+						}
+						Console.SetCursorPosition(pelletX, pelletY);
+						Console.ForegroundColor = ConsoleColor.Cyan;
+						Console.BackgroundColor = bgColor;
+						Console.Write("#");
+						pelletOn = true;
 
-                    }
+					}
                     Array.Resize<int>(ref xPoints, snakeLength);
                     Array.Resize<int>(ref yPoints, snakeLength);
 
-                    System.Threading.Thread.Sleep(delay);
+					Thread.Sleep(delay);
                     colourTog++;
                     if (Console.KeyAvailable)
                     {
@@ -118,8 +106,6 @@ namespace ConsoleGraphics
                                 {
                                     direction = "down";
                                 }
-                                break;
-                            default:
                                 break;
                         }
                     } //Inputs & direction
@@ -156,7 +142,7 @@ namespace ConsoleGraphics
                     {
                         Console.SetCursorPosition(xPoints[0], yPoints[0]);
                     }
-                    catch (System.ArgumentOutOfRangeException)
+                    catch (ArgumentOutOfRangeException)
                     {
                         alive = false;
                     }
@@ -176,7 +162,7 @@ namespace ConsoleGraphics
                     {
                         Console.SetCursorPosition(xPoints[xPoints.Length - 1], yPoints[yPoints.Length - 1]);
                     }
-                    catch (System.ArgumentOutOfRangeException)
+                    catch (ArgumentOutOfRangeException)
                     {
                         alive = false;
                     }
@@ -193,10 +179,7 @@ namespace ConsoleGraphics
 
                     for (int l = (xPoints.Length - 1); l > 1; l--)
                     {
-                        if (xPoints[l] == xPoints[0] & yPoints[l] == yPoints[0])
-                        {
-                            alive = false;
-                        }
+						alive &= !(xPoints[l] == xPoints[0] & yPoints[l] == yPoints[0]);
 
                     }
                     score = ((snakeLength) - 8);
@@ -209,57 +192,26 @@ namespace ConsoleGraphics
                 new Thread(() => Console.Beep(37, 1)).Start();
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
-                /*
-
-                Console.Beep(2093, 260);
-
-                Console.Beep(1319, 260);
-
-                Console.Beep(1047, 260);
-
-                Console.Beep(1319, 260);
-
-                Console.Beep(1760, 260);
-
-                Console.Beep(1319, 260);
-
-                Console.Beep(932, 260);
-
-                Console.Beep(784, 260);
-               
-
-                Console.Beep(2093, 200);
-
-                Console.Beep(1319, 200);
-
-                Console.Beep(1047, 200);
-
-                Console.Beep(1319, 200);
-
-                Console.Beep(1760, 200);
-
-                Console.Beep(1319, 200);
-                */
-
+                
                 Console.Beep(831, 250);
                 
-
                 Console.Beep(785, 250);
 
-                ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+                ConsoleColor[] colors = (ConsoleColor[])Enum.GetValues(typeof(ConsoleColor));
 
                 for (int i = 0; i < 1; i++)
                 {
-                    foreach (var color in colors)
-                    {
-                        Console.SetCursorPosition(0, 0);
-                        Console.ForegroundColor = color;
-                        Console.Clear();
-                        Console.WriteLine("\n\n\n\n\n");
-                        Console.WriteLine("\n                       Game over :(");
-                        Console.WriteLine("\n\n                   Your score was: {0} !", score);
-                        System.Threading.Thread.Sleep(100);
-                    }
+					for (int j = 0, colorsLength = colors.Length; j < colorsLength; j++)
+					{
+						var color = colors[j];
+						Console.SetCursorPosition(0, 0);
+						Console.ForegroundColor = color;
+						Console.Clear();
+						Console.WriteLine("\n\n\n\n\n");
+						Console.WriteLine("\n                       Game over :(");
+						Console.WriteLine("\n\n                   Your score was: {0} !", score);
+						Thread.Sleep(100);
+					}
                 }
                 Thread.Sleep(1000);
                 Console.WriteLine("\n\n\n\n\n\n             -- Press Any Key To Continue --");
@@ -271,5 +223,3 @@ namespace ConsoleGraphics
         }
     }
 }
-
-
